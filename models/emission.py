3f66caa4e532befc3a0577158e027f34923d6a22
Paras Sharma <realparas52@gmail.com>
2026-08-30 02:00:32 +0530

    Delete models directory

--- models/emission.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class EmissionModel(BaseModel):
-    baseline_co2_tonnes_year: float
-    pathway_co2_tonnes_year: float
-    reduction_pct: float
-    grid_emission_factor_used: float
